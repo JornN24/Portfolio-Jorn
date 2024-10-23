783e9a0f253bc2872afe6c3744e2f5278424c812
Jorn Noten <j.noten@sintlucasedu.nl>
2024-10-23 10:09:49 +0200

    fix route

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './Router/index.js';
 import './components/main.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
