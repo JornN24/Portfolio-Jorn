5fd47c93819f34356a8fa828ca6ee10faad13507
Jorn Noten <j.noten@sintlucasedu.nl>
2024-12-16 11:43:11 +0100

    Portfolio
    The portfolio how it is now

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './Router/index.js';
-import './components/main.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
